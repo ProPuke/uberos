8c963a8421bd5d024d1c3680e265240840f60c77
ProPuke <ProPuke@31i73.com>
2025-01-31 19:55:52 +0000

    tidy

--- tools/pack-image.sh
+++ tools/pack-image.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-echo --allow-read="$1" --allow-write="$1" $(dirname "$(realpath "$0")")/pack-image.ts "$@"
 deno run --allow-read="$1" --allow-write="$1" $(dirname "$(realpath "$0")")/pack-image.ts "$@"
