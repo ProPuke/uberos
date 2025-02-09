255e5d63e89aab6d1f3297806c51bc8055d0e000
ProPuke <ProPuke@31i73.com>
2025-02-09 15:35:18 +0000

    tidy

--- kernel/arch/x86-ibm-bios/kernel.cpp
+++ kernel/arch/x86-ibm-bios/kernel.cpp
@@ -17,39 +17,49 @@
 extern U8 __start;
 extern U8 __end;
 
+namespace {
+	void init_multiboot(unsigned long magic, multiboot_info *multiboot) {
+		if(magic!=MULTIBOOT_BOOTLOADER_MAGIC) return;
+
+		{ // serial debug
+			// cmdline passed?
+			if(multiboot->flags & 1<<2){
+				//TODO: if "serial_debug" is present, hook stdout to COM1, 8-N-1 57600 baud
+				(void)multiboot->cmdline;
+			}
+		}
+
+		{ // find memory
+			U64 maxAddress = 0x0;
+
+			// find the available memory the kernel sits within
+			for(
+				auto mmap=(multiboot_memory_map_t*)multiboot->mmap_addr;
+				mmap<mmap+multiboot->mmap_length;
+				mmap = (multiboot_memory_map_t*)((size_t)mmap + sizeof(mmap->size) + mmap->size)
+			) {
+				maxAddress = mmap->addr+mmap->len;
+
+				if(mmap->type!=MULTIBOOT_MEMORY_AVAILABLE) continue;
+
+				if(mmap->addr<=(U64)&__start && mmap->addr+mmap->len>=(U64)&__end){
+					memory::heapSize = (U8*)(mmap->addr+mmap->len) - (U8*)memory::heap;
+					break;
+				}
+			}
+
+			memory::totalMemory = maxAddress+1;
+		}
+	}
+}
+
 namespace arch {
 	namespace x86_ibm_bios {
 		namespace kernel {
 			extern "C" void entrypoint(unsigned long magic, multiboot_info *multiboot) {
 				::arch::x86::cpuInfo::enable_sse(); //mmx/sse instructions may be present in startup code, so get this in early
 
-				if(magic==MULTIBOOT_BOOTLOADER_MAGIC){
-					// cmdline passed?
-					if(multiboot->flags & 1<<2){
-						//TODO: if "serial_debug" is present, hook stdout to COM1, 8-N-1 57600 baud
-						(void)multiboot->cmdline;
-					}
-
-					U64 maxAddress = 0x0;
-
-					// find the available memory the kernel sits within
-					for(
-						auto mmap=(multiboot_memory_map_t*)multiboot->mmap_addr;
-						mmap<mmap+multiboot->mmap_length;
-						mmap = (multiboot_memory_map_t*)((size_t)mmap + sizeof(mmap->size) + mmap->size)
-					) {
-						maxAddress = mmap->addr+mmap->len;
-
-						if(mmap->type!=MULTIBOOT_MEMORY_AVAILABLE) continue;
-
-						if(mmap->addr<=(U64)&__start && mmap->addr+mmap->len>=(U64)&__end){
-							memory::heapSize = (U8*)(mmap->addr+mmap->len) - (U8*)memory::heap;
-							break;
-						}
-					}
-
-					memory::totalMemory = maxAddress+1;
-				}
+				init_multiboot(magic, multiboot);
 
 				::kernel::run();
 			}
@@ -61,19 +71,9 @@ namespace kernel {
 	void _preInit() {
 		arch::x86_ibm::stdout::init();
 		exceptions::init();
-		// for(auto &driver:drivers::iterate<Driver>()){
-		// 	logging::print_info_start();
-		// 	logging::print_inline(driver.type->name, " (", driver.type->description, ")");
-		// 	for(auto parent = driver.type->parentType; parent&&parent->parentType; parent = parent->parentType) {
-		// 		logging::print_inline(" / ", parent->description);
-		// 	}
-		// 	logging::print_end();
-		// 	// logging::print_info(driver.type->name, " (", driver.type->description, ") - ", driver.type->pretty_typename());
-		// }
 		#ifdef KERNEL_MMU
 			mmu::init();
 		#endif
-		// debug::halt();
 	}
 
 	void _postInit() {
