68739925349079ccb31ebe8d9532b750483ab773
ProPuke <ProPuke@31i73.com>
2025-02-27 23:11:20 +0000

    improved: llist debuggering

--- common/debugUtils.hpp
+++ common/debugUtils.hpp
@@ -11,22 +11,43 @@ void debug_llist(LList<Type> &list, const char *label = "list") {
 	unsigned length = 0;
 	LListItem<Type> *last = nullptr;
 	for(auto item=list.head; item; item=item->next){
-		if(last&&item->prev!=last){
-			logging::print_error("Error: Item ", item, " has missing prev record");
+		if(item->prev!=last){
+			logging::print_error("Error: Item ", item, " has incorrect prev record when walking forwards");
+			debug::halt();
 		}
 		length++;
 		last = item;
 	}
 
 	if(length!=list.size){
-		logging::print_error("Error: Walked length of ", length, " did not match expected size of ", list.size);
+		logging::print_error("Error: Forward walked length of ", length, " did not match expected size of ", list.size);
+		debug::halt();
 	}
 
-	logging::print_info("size: ", list.size,  " / ", length);
-	logging::print_info("head: ", list.head);
-	logging::print_info("tail: ", list.tail);
-
-	for(auto item=list.head; item; item=item->next){
-		logging::print_info("item: ", item, " prev = ", item->prev, " next = ", item->next);
+	if(last!=list.tail){
+		logging::print_error("Error: Last forward item ", last, " does not match the tail ", list.tail);
+		debug::halt();
 	}
+
+	last = nullptr;
+	for(auto item=list.tail; item; item=item->prev){
+		if(item->next!=last){
+			logging::print_error("Error: Item ", item, " has incorrect next record when walking backwards");
+			debug::halt();
+		}
+		last = item;
+	}
+
+	if(last!=list.head){
+		logging::print_error("Error: Last backward item ", last, " does not match the head ", list.head);
+		debug::halt();
+	}
+
+	// logging::print_info("size: ", list.size,  " / ", length);
+	// logging::print_info("head: ", list.head);
+	// logging::print_info("tail: ", list.tail);
+
+	// for(auto item=list.head; item; item=item->next){
+	// 	logging::print_info("item: ", item, " prev = ", item->prev, " next = ", item->next);
+	// }
 }
