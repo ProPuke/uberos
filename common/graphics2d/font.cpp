2198bd9dc1b217ded3f544f2982b12d9bfabdbef
ProPuke <ProPuke@31i73.com>
2025-03-02 19:21:16 +0000

    added: OpenSans bold font

--- common/graphics2d/font.cpp
+++ common/graphics2d/font.cpp
@@ -3,6 +3,7 @@
 namespace graphics2d {
 	namespace font {
 		Font *default_sans = &openSans;
+		Font *default_sans_bold = &openSans_bold;
 		Font *default_console = &roboto_Mono;
 	}
 }
--- common/graphics2d/font.hpp
+++ common/graphics2d/font.hpp
@@ -5,11 +5,13 @@
 namespace graphics2d {
 	namespace font {
 		extern Font openSans;
+		extern Font openSans_bold;
 		extern Font roboto_Mono;
 		extern Font manrope;
 		extern Font manrope_extraBold;
 
 		extern Font *default_sans;
+		extern Font *default_sans_bold;
 		extern Font *default_console;
 	}
 }
