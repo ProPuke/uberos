74b7393be37fb2eac8b37eb281cadd695634e779
ProPuke <ProPuke@31i73.com>
2025-04-18 23:29:30 +0100

    changed: font test now shows font sizes and starts with increments of 1

--- kernel/tests/fontTest.cpp
+++ kernel/tests/fontTest.cpp
@@ -19,28 +19,39 @@ namespace tests::fontTest {
 		window = &desktopManager->create_standard_window("Font Test", 320, 320);
 		// view = graphics2d::create_view(nullptr, graphics2d::DisplayLayer::topMost, margin, margin, min(1300u, framebuffer.buffer.width-margin*2), 256);
 
-		static auto scale = 30u;
+		// static auto scale = 30u;
+		static auto scale = 1u;
 
 		static auto redraw = [](){
 			auto &clientArea = window->get_client_area();
 
 			clientArea.draw_rect(0, 0, window->get_width(), window->get_height(), window->get_background_colour());
 
-			auto fontSettings = graphics2d::Buffer::FontSettings{
+			auto sizeFontSettings = graphics2d::Buffer::FontSettings{
 				.font = *graphics2d::font::default_sans,
-				.size = scale
+				.size = 10,
+				.maxLines = 1
 			};
 
-			for(auto y=scale;y<(U32)window->get_height();y++){
+			auto sizeWidth = clientArea.measure_text(sizeFontSettings, "888 ").blockWidth;
+
+			auto fontSettings = graphics2d::Buffer::FontSettings{
+				.font = *graphics2d::font::default_sans,
+				.size = scale,
+				.maxLines = 1
+			};
+
+			for(auto y=max(sizeFontSettings.size, fontSettings.size);y<(U32)window->get_height();y++){
 				// if(fontSettings.size>24){
-				// 	clientArea.draw_text(fontSettings, "Abc", 10+1, y+1, 320, 0x222222);
-				// 	clientArea.draw_text(fontSettings, "Abc", 10, y, 320, 0xaaaaaa);
+				// 	clientArea.draw_text(fontSettings, "Abc", 10+1, y+1, clientArea.width, 0x222222);
+				// 	clientArea.draw_text(fontSettings, "Abc", 10, y, clientArea.width, 0xaaaaaa);
 				// }else{
-					clientArea.draw_text(fontSettings, "Abc", 10, y, 320, 0x444444);
+					clientArea.draw_text(sizeFontSettings, to_string(fontSettings.size), 10, y, clientArea.width, 0x444444);
+					clientArea.draw_text(fontSettings, "Abc", 10+sizeWidth, y, clientArea.width, 0x444444);
 				// }
 
 				fontSettings.size += scale;
-				y += fontSettings.size * 4 / 5;
+				y += max(sizeFontSettings.size, fontSettings.size) * 4 / 5;
 			}
 
 			static char statusBuffer[64];
@@ -59,7 +70,7 @@ namespace tests::fontTest {
 				redraw();
 
 			}else if(event.type==driver::DesktopManager::Window::Event::Type::mouseScrolled){
-				scale = maths::clamp(scale - event.mouseScrolled.distance*(scale>20?3:scale>16?2:1), 1u, 60u);
+				scale = maths::clamp((I32)scale - event.mouseScrolled.distance*(scale>20?3:scale>16?2:1), 1, 60);
 				redraw();
 			}
 
